929dddc77b89cf82774385eb0d022c668aef75f1
Prateek Mohapatra <prateekmsoa@gmail.com>
2025-03-31 00:20:21 +0530

    added nexafit details

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -1,34 +1,41 @@
 const projects = [
-    {
+        {
         id: 1,
+        title: 'nexaFit',
+        description: 'A free web app that helps users plan healthy meals and track calories burned after workouts using smart predictions.',
+        image: '/projects/nexafit.webp',
+        link: 'https://nexafit-zeta.vercel.app',
+        },
+        {
+        id: 2,
         title: 'mindEase',
         description: 'A 24/7 mental health support platform that blends AI therapy with seamless ease of use',
         image: '/projects/mindease.webp',
         link: 'https://mindease-eight.vercel.app',
         },
         {
-        id: 2,
+        id: 3,
         title: 'Fuzzy Extropy',
         description: 'Brought entropy to order by turbocharging decision trees with a cutting-edge, probabilistic impurity metric.',
         image: '/projects/extropy.webp',
         link: 'https://github.com/ezahpizza/scikit-learn',
         },
         {
-        id: 3,
+        id: 4,
         title: 'BERT Question-Answerer',
         description: 'Made machines understand and answer with human-like precision using the power of BERT and custom NLP finetuning.',
         image: '/projects/qa.webp',
         link: 'https://ezahpizza-bert-q-a.streamlit.app',
         },
         {
-        id: 4,
+        id: 5,
         title: 'T5 Text Translator',
         description: 'Bridged language gaps with a custom-built AI translator that speaks fluent Español with elegance and accuracy.',
         image: '/projects/translator.webp',
         link: 'https://ezahpizza-text-translation.streamlit.app',
         },
         {
-        id: 5,
+        id: 6,
         title: 'T5 Text Summarizer',
         description: 'Finetuned an AI model to slice through verbose content and return clean, coherent summaries that actually make sense.',
         image: '/projects/summariser.webp',
